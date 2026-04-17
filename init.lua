a8144706023e5029f11ba3150f488a91a8b39f7e
Andrew S. Parnell <aparnell@gmail.com>
2026-04-17 11:39:50 -0700

    Added documentation comments, moved sendRequest to a table based parameter

--- init.lua
+++ init.lua
@@ -64,7 +64,13 @@ local connectToTeams = function() end
 local requestID = 0
 local function sendRequest(msg)
    requestID = requestID + 1
-   local requestMsg = string.format('{"requestId":%s, %s}', requestID, msg)
+
+   local requestMsg = hs.json.encode({
+      requestId = requestID,
+      action = msg.action or "",
+      parameters = msg.parameters or {}
+   });
+
    if teamsWebsocket then
       teamsWebsocket:send(requestMsg)
    end
@@ -241,9 +247,25 @@ end
 
 
 -- query-state
+
+--- MSTeams:queryState()-> spoon.MSTeams 
+--- Method
+--- Sends a query-state command to Microsoft Teams. 
+--- Teams should then subsequently send a meetingUpdate message
+--- with meetingPermissions and, if the spoon has been paired, meetingState 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:queryState()
-   if running then
-      sendRequest('"action":"query-state","parameters":{}')
+   if not running then
+      MSTeams.logger.w("spoon MSTeams must be running to perform actions. Run MSTeams:start()")
+   elseif not teamsWebsocket then
+      MSTeams.logger.w("Not connected to teams")
+   else
+      sendRequest({action="query-state",parameters={}})
    end
 
    return self
@@ -287,9 +309,19 @@ local function canAct(requiredPermission)
 end
 
 -- pair
+
+--- MSTeams:pair()-> spoon.MSTeams 
+--- Method
+--- If able: sends a pair command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:pair()
    if canAct(permission.canPair) then
-      sendRequest('"action":"pair","parameters":{}')
+      sendRequest({action="pair", parameters={}})
    end
 
    return self
@@ -297,27 +329,57 @@ end
 
 
 -- toggle-mute
+
+--- MSTeams:toggleMute()-> spoon.MSTeams 
+--- Method
+--- If able: sends a toggle-mute command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:toggleMute()
    if canAct(permission.canToggleMute) then
-      sendRequest('"action":"toggle-mute","parameters":{}')
+      sendRequest({action="toggle-mute", parameters={}})
    end
 
    return self
 end
 
 -- mute
+
+--- MSTeams:mute()-> spoon.MSTeams 
+--- Method
+--- If able: sends a mute command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:mute()
    if canAct(permission.canToggleMute) then
-      sendRequest('"action":"mute","parameters":{}')
+      sendRequest({action="mute", parameters={}})
    end
 
    return self
 end
 
 -- unmute
+
+--- MSTeams:unmute()-> spoon.MSTeams 
+--- Method
+--- If able: sends a unmute command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:unmute()
    if canAct(permission.canToggleMute) then
-      sendRequest('"action":"unmute","parameters":{}')
+      sendRequest({action="unmute", parameters={}})
    end
 
    return self
@@ -326,27 +388,57 @@ end
 
 
 -- toggle-video
+
+--- MSTeams:toggleVideo()-> spoon.MSTeams 
+--- Method
+--- If able: sends a toggle-video command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:toggleVideo()
    if canAct(permission.canToggleVideo) then
-      sendRequest('"action":"toggle-video","parameters":{}')
+      sendRequest({action="toggle-video", parameters={}})
    end
 
    return self
 end
 
 -- show-video
+
+--- MSTeams:showVideo()-> spoon.MSTeams 
+--- Method
+--- If able: sends a show-video command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:showVideo()
    if canAct(permission.canToggleVideo) then
-      sendRequest('"action":"show-video","parameters":{}')
+      sendRequest({action="show-video", parameters={}})
    end
 
    return self
 end
 
 -- hide-video
+
+--- MSTeams:hide-video()-> spoon.MSTeams 
+--- Method
+--- If able: sends a hide-video command to Microsoft Teams.
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:hideVideo()
    if canAct(permission.canToggleVideo) then
-      sendRequest('"action":"hide-video","parameters":{}')
+      sendRequest({action="hide-video", parameters={}})
    end
 
    return self
@@ -354,36 +446,76 @@ end
 
 
 -- stop-sharing
+
+--- MSTeams:stopSharing()-> spoon.MSTeams 
+--- Method
+--- If able: sends a stop-sharing command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:stopSharing()
    if canAct(permission.canStopSharing) then
-      sendRequest('"action":"stop-sharing","parameters":{}')
+      sendRequest({action="stop-sharing", parameters={}})
    end
    return self
 end
 
 
 -- toggle-background-blur
+
+--- MSTeams:toggleBlurBackground()-> spoon.MSTeams 
+--- Method
+--- If able: sends a toggle-background-blur command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:toggleBlurBackground()
    if canAct(permission.canToggleBlur) then
-      sendRequest('"action":"toggle-background-blur","parameters":{}')
+      sendRequest({action="toggle-background-blur", parameters={}})
    end
 
    return self
 end
 
 -- blur-background
+
+--- MSTeams:blurBackground()-> spoon.MSTeams 
+--- Method
+--- If able: sends a blur-background command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:blurBackground()
    if canAct(permission.canToggleBlur) then
-      sendRequest('"action":"blur-background","parameters":{}')
+      sendRequest({action="blur-background", parameters={}})
    end
 
    return self
 end
 
 -- unblur-background
+
+--- MSTeams:unblurBackground()-> spoon.MSTeams 
+--- Method
+--- If able: sends a unblur-background command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:unblurBackground()
    if canAct(permission.canToggleBlur) then
-      sendRequest('"action":"unblur-background","parameters":{}')
+      sendRequest({action="unblur-background", parameters={}})
    end
 
    return self
@@ -391,36 +523,76 @@ end
 
 
 -- toggle-hand
+
+--- MSTeams:toggleHand()-> spoon.MSTeams 
+--- Method
+--- If able: sends a toggle-hand command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:toggleHand()
    if canAct(permission.canToggleHand) then
-      sendRequest('"action":"toggle-hand","parameters":{}')
+      sendRequest({action="toggle-hand", parameters={}})
    end
 
    return self
 end
 
 -- raise-hand
+
+--- MSTeams:raiseHand()-> spoon.MSTeams 
+--- Method
+--- If able: sends a raise-hand command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:raiseHand()
    if canAct(permission.canToggleHand) then
-      sendRequest('"action":"raise-hand","parameters":{}')
+      sendRequest({action="raise-hand", parameters={}})
    end
 
    return self
 end
 
 -- lower-hand
+
+--- MSTeams:lowerHand()-> spoon.MSTeams 
+--- Method
+--- If able: sends a lower-hand command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:lowerHand()
    if canAct(permission.canToggleHand) then
-      sendRequest('"action":"lower-hand","parameters":{}')
+      sendRequest({action="lower-hand", parameters={}})
    end
 
    return self
 end
 
 -- send-reaction {"type":"like"}
+
+--- MSTeams:reactLike()-> spoon.MSTeams 
+--- Method
+--- If able: sends a send-reaction command of type:"like" to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:reactLike(reaction)
    if canAct(permission.canReact) then
-      sendRequest('"action":"send-reaction","parameters":{"type":"like"}')
+      sendRequest({action="send-reaction", parameters={type="like"}})
    end
 
    return self
@@ -428,61 +600,151 @@ end
 
 
 -- send-reaction {"type":"love"}
+
+--- MSTeams:reactLove()-> spoon.MSTeams 
+--- Method
+--- If able: sends a send-reaction command of type:"love" to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:reactLove(reaction)
    if canAct(permission.canReact) then
-      sendRequest('"action":"send-reaction","parameters":{"type":"love"}')
+      sendRequest({action="send-reaction", parameters={type="love"}})
    end
 
    return self
 end
 
 -- send-reaction {"type":"applause"}
+
+--- MSTeams:reactApplause()-> spoon.MSTeams 
+--- Method
+--- If able: sends a send-reaction command of type:"applause" to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:reactApplause(reaction)
    if canAct(permission.canReact) then
-      sendRequest('"action":"send-reaction","parameters":{"type":"applause"}')
+      sendRequest({action="send-reaction", parameters={type="applause"}})
    end
 
    return self
 end
 
 -- send-reaction {"type":"laugh"}
+
+--- MSTeams:reactLaugh()-> spoon.MSTeams 
+--- Method
+--- If able: sends a send-reaction command of type:"laugh" to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:reactLaugh(reaction)
    if canAct(permission.canReact) then
-      sendRequest('"action":"send-reaction","parameters":{"type":"laugh"}')
+      sendRequest({action="send-reaction", parameters={type="laugh"}})
+   end
+
+   return self
+end
+
+-- send-reaction {"type":"wow"}
+
+--- MSTeams:reactWow()-> spoon.MSTeams 
+--- Method
+--- If able: sends a send-reaction command of type:"wow" to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
+function MSTeams:reactWow(reaction)
+   if canAct(permission.canReact) then
+      sendRequest({action="send-reaction", parameters={type="wow"}})
    end
 
    return self
 end
 
 -- leave-call
+
+--- MSTeams:leaveCall()-> spoon.MSTeams 
+--- Method
+--- If able: sends a leave-call command to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:leaveCall()
    if canAct(permission.canLeave) then
-      sendRequest('"action":"leave-call","parameters":{}')
+      sendRequest({action="leave-call", parameters={}})
    end
 
    return self
 end
 
 -- toggle-ui {"type":"chat"}
+
+--- MSTeams:toggleChat()-> spoon.MSTeams 
+--- Method
+--- If able: sends a toggle-ui command of type:"chat" to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:toggleChat()
    if canAct(permission.canToggleChat) then
-         sendRequest('"action":"toggle-ui","parameters":{"type":"chat"}')
+         sendRequest({action="toggle-ui", parameters={type="chat"}})
    end
    return self
 end
 
--- toggle-ui {"type":"share tray"}
+-- toggle-ui {"type":"share-tray"}
+
+--- MSTeams:toggleShareTray()-> spoon.MSTeams 
+--- Method
+--- If able: sends a toggle-ui command of type:"share-tray" to Microsoft Teams. 
+---
+--- Parameters:
+---  * None
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
 function MSTeams:toggleShareTray()
    if canAct(permission.canToggleShareTray) then
-         sendRequest('"action":"toggle-ui","parameters":{"type":"share tray"}')
+         sendRequest({action="toggle-ui", parameters={type="share-tray"}})
    end
    return self
 end
 
 
-function MSTeams:customRequest(msg)
+
+--- MSTeams:customRequest(action[, parameters])-> spoon.MSTeams 
+--- Method
+--- If able: sends a custom command to Microsoft Teams. 
+---
+--- Parameters:
+---  * action - string of the action to send to teams
+---  * parameters - (optional) table containing parameters to include in the command
+---
+--- Returns:
+---  * The spoon.MSTeams object for chaining
+function MSTeams:customRequest(action, parameters)
    if canAct() then
-      sendRequest(msg)
+      sendRequest({action=action, parameters=parameters or {}})
    end
 
    return self
